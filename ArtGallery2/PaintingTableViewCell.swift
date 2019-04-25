2e8161d91f98607f63bc5f2c287bb3f6e7dabf09
Michael R.F <mrflowers22@uwalumni.com>
2019-04-25 07:54:34 -0400

    added table view and applicable source code files.

---
+++ ArtGallery2/PaintingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PaintingTableViewCell.swift
+//  ArtGallery2
+//
+//  Created by Michael Flowers on 4/25/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class PaintingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ArtGallery2/PaintingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PaintingViewController.swift
+//  ArtGallery2
+//
+//  Created by Michael Flowers on 4/25/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class PaintingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
